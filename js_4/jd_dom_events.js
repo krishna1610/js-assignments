32283cdbe3a6952b3f49c0f12097192ee5b38668
Krishna <krishna@Krishnas-MBP.lan1>
2020-07-03 22:15:10 -0700

    add new work

---
+++ js_4/jd_dom_events.js
@@ -0,0 +1,72 @@
+// problem 1
+        // function temconvetor() {
+        //     let element = document.getElementById("p1");
+        //     let fahrenheit = element.value;
+        //     if (isNaN(fahrenheit)) {
+        //         element.style.borderColor = "#f51707";
+        //         prompt("enter correct values");
+        //     } else {
+        //         let celsius = (fahrenheit - 32) * (5 / 9);
+        //         document.getElementById("cel").innerText = Math.round(celsius);
+        //     }
+        // }
+
+        // problem 2
+        // function loadfun() {
+        //     alert("welcome to my blog");
+        // }
+
+
+        // problem 3
+        // function hide() {
+
+        //     // var elements = document.getElementsByClassName("otherEmts");
+        //     // for (var i = 0; i < elements.length; i++) {
+        //     //     //elements[i].style.visibility = "hidden"; // or
+        //     //     elements[i].style.display = "none"; // depending on what you're doing
+        //     // }
+
+
+        //     document.children // head,body
+        //     var x = document.getElementsByTagName("body");
+        //     var y = x[0];
+        //     var z = y.children;
+        //     for (let i = 0; i < z.length; i++) {
+        //         if (z[i] == document.getElementById("hideElems") || z[i] == document.getElementById("showElems")) {
+        //             z[i].style.display = "block";
+
+        //         } else {
+        //             z[i].style.display = "none";
+        //         }
+        //     }
+
+        // }
+
+
+        // problem 4
+        // function show(){
+        //     document.children;
+        //     var x = document.getElementsByTagName("body");
+        //     var y = x[0];
+        //     var z = y.children;
+        //     for(let i=0;i<z.length - 2;i++){ // use -2 because to hide script tag stuff
+               
+        //         z[i].style.display = "block";  
+        //     }
+          
+        // }
+
+        // problem 5
+    
+        function over(){
+           
+        }
+        function out(){
+           
+        }
+    
+        // problem 6
+        // problem 7
+        // problem 8
+        // problem 9
+        // problem 10
